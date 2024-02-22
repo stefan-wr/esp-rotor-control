54eb3151e8d7f32aff2eaecb7a3e2115bbf04e85
Stefan Wraase <dev@stfwrs.de>
2024-02-22 22:49:03 +0100

    Vue-app now compiles to a single index.html file.

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteSingleFile(),
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
     })
